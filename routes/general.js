4e550b9042ee2c22d8d988feb2ba2678b22305cc
Gemechu <95497037+Gesifeta@users.noreply.github.com>
2024-09-28 11:39:19 +0300

    improved the API call with Promise

--- routes/general.js
+++ routes/general.js
@@ -2,11 +2,10 @@ import express from "express"
 
 import { books } from "../data/booksdb.js";
 import { users } from "../data/usersdb.js";
-import { userExists,authenticateUser } from "../auth/auth.js";
+import { userExists, authenticateUser } from "../auth/auth.js";
 import { generateToken, verifyToken } from "../auth/token.js";
 
 export const public_users = express.Router();
-
 
 public_users.post("/customer/register", (req, res) => {
   if (!req.body) {
@@ -50,33 +49,49 @@ public_users.post("/customer/login", (req, res) => {
   return res.status(404).json({ message: "User not found" });
 })
 // Get the book list available in the shop
-public_users.get('/', function (req, res) {
-  let data = Object.keys(books).map(key => {
-    return books[key]
+public_users.get('/', async function (req, res) {
+  let result = new Promise((resolve, reject) => {
+    resolve(Object.keys(books).map(key => {
+      return books[key]
+    }))
+    reject("Error")
   })
-
+  let data = await result;
   return res.status(300).json({ data });
-});
+})
+
+
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn', function (req, res) {
-
+public_users.get('/isbn/:isbn', async function (req, res) {
   let isbn = req.params.isbn;
-  const data = Object.values(books).filter(book => book.ISBN === isbn)
+  const result = new Promise((resolve, reject) => {
+    resolve(Object.values(books).filter(book => book.ISBN === isbn))
+    reject("Error")
+  })
+  const data = await result;
   return res.status(200).json(data);
 });
 
 // Get book details based on author
-public_users.get('/author/:author', function (req, res) {
+public_users.get('/author/:author', async function (req, res) {
   let author = req.params.author;
-  const data = Object.values(books).filter(book => book.author === author)
+  const result = new Promise((resolve, reject) => {
+    resolve(Object.values(books).filter(book => book.author === author))
+    reject("Error")
+  })
+  const data = await result;
   return res.status(200).json(data);
 });
 
 // Get all books based on title
-public_users.get('/title/:title', function (req, res) {
+public_users.get('/title/:title', async function (req, res) {
   let title = req.params.title;
-  const data = Object.values(books).filter(book => book.title === title)
+  let result = new Promise((resolve, reject) => {
+    resolve(Object.values(books).filter(book => book.title === title))
+    reject("Error")
+  })
+  const data = await result;
   return res.status(200).json(data);
 });
 
